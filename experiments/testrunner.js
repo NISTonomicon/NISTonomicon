8cf9ece0f31cf045b295d777958b978dd9b86a27
hortinstein <hortinstein@gmail.com>
2015-11-25 15:50:19 +0000

    got overlays working
    "

--- experiments/testrunner.js
+++ experiments/testrunner.js
@@ -1,74 +1,73 @@
 process.env.NODE_ENV = 'test';
 var control_list = require('../util/testGenerator.js')
-var placeholder = function(done) {
+//this function takes the overlay, inherited tests and implemented tests and builds a dictionary 
+var assembleTestDict = function(overlay, inheritedTests, implementedTests) {
+    var test_dict = {}
+    for(control in overlay) {
+        control_name = overlay[control]
+        if(inheritedTests.hasOwnProperty(control_name)) { //checks first for inherited tests
+            test_dict[control_name] = inheritedTests[control_name];
+        } else if(implementedTests.hasOwnProperty(control_name)) { //checks for implemented tests
+            test_dict[control_name] = implementedTests[control_name];
+        } else {
+            test_dict[control_name] = pending //adds in pending for potential new tests
+        }
+    }
+    return test_dict
+}
+var pruneControlList = function(test_dict, implemented_dict) {}
+var executeTest = function(test_name, test) {
+    if (test){
+        it(test_name,test)
+    }
+}
+var executeEnhancements = function(enhancements, test_dict) {
+    for(enhancement in enhancements) {
+        enhancement_title = enhancements[enhancement]
+        executeTest(enhancement + " " + enhancement_title,test_dict[enhancement])
+    }
+}
+var executeControl = function(control, control_number, test_dict) {
+    if(control.hasOwnProperty('enhancements')) { //checking to see if there are any enhancements
+        executeTest(control_number + " " + control.title,test_dict[control_number]) //this is the base control
+        executeEnhancements(control['enhancements'], test_dict)
+    } else {
+        executeTest(control_number + " " + control.title,test_dict[control_number]) //this is the base control
+    }
+}
+var executeFamily = function(family, test_dict) {
+    for(control in family) { //for all of the controls in each family
+        control_title = family[control].title
+        describe(control_title, function() {
+            executeControl(family[control], control, test_dict)
+        });
+    }
+}
+startTests = function(control_list, test_dict) {
+    for(family in control_list) { //for each of the 18 families in the controls 
+        describe(family, function() {
+            executeFamily(control_list[family], test_dict)
+        })
+    }
+};
+var pending = function(done) {
     this.skip('works', function() {
         // nothing yet
     });
 }
-var t1 = function(done) {
+var passing = function(done) {
     done()
 }
-var t2 = function(done) {
-    done()
+var fail = function(done) {
+    throw "bad!";
 }
-//this is a dictionary where all of the tests will be stored
-test_dict = {}
-//this function takes the
-var assembleTestDict = function(overlay, inheritedTests, implementedTests, callback) {
-    for(control in overlay) {
-        
-        control_name = overlay[control]
-       
-        if(inheritedTests.hasOwnProperty(control_name)) { //checks first for inherited tests
-            test_dict[control_name] = inheritedTests[control_name];
-        } else if(implementedTests.hasOwnProperty(control)) { //checks for implemented tests
-            test_dict[control_name] = implementedTests[control_name];
-        } else {
-            test_dict[control_name] = placeholder //adds in pending for potential new tests
-        }
-    }
-    callback()
-}
-var executeTest = function(test_name) {
-    console.log(test_dict)
-    console.log(test_dict[test_name])
-    return test_dict[test_name]
-}
-startTests = function(overlay, inheritedTests, implementedTests) {
-    before(function(done) {
-        assembleTestDict(overlay, inheritedTests, implementedTests,done)
-        console.log(test_dict)
-    });
-    it('should', executeTest('t1'))
-    //     for(family in control_list) { //for each of the 18 families in the controls 
-    //         describe(family, function() { //create a sub catagory
-    //             for(control in control_list[family]) { //for all of the controls in each family
-    //                 control_title = control_list[family][control].title
-    //                 //TODO CHECK if in overlay here, if not skip! 
-    //                 /////////
-    //                 if(control_list[family][control].hasOwnProperty('enhancements')) { //checking to see if there are any enhancements
-    //                     describe(control + " " + control_title, function() {
-    //                         it(control, executeTest('t1',test_dict)) //this is the base control
-    //                         describe('enhancements', function() {
-    //                             for(enhancement in control_list[family][control]['enhancements']) {
-    //                                 //TODO CHECK if in overlay here, if not skip! 
-    //                                 enhancement_title = control_list[family][control]['enhancements'][enhancement]
-    //                                 it(enhancement + " " + enhancement_title, executeTest('t1',test_dict))
-    //                             }
-    //                         });
-    //                     });
-    //                 } else {
-    //                     it(control, executeTest('t1',test_dict)) //this is the base control
-    //                 }
-    //             }
-    //         });
-    //     }
-};
-overlay = ['t1', 't2', 't4']
+overlay = ['SI_8_1', 'PM_6', 'SC_8_1']
 inherited_dict = {
-    't1': t1
+    'SI_8_1': passing
 }
 implemented_dict = {
-    't2': t1
+    'PM_6': fail
 }
-startTests(overlay, inherited_dict, implemented_dict);
+var test_dict = assembleTestDict(overlay, inherited_dict, implemented_dict)
+console.log(test_dict)
+startTests(control_list, test_dict);
