e28108c1ff1dc8207bff061f7bcf8964bc5d607c
hortinstein <hortinstein@gmail.com>
2015-12-23 19:49:17 +0000

    forgot to add the test file

---
+++ test/testcase_function_configs/testcase_inherited_and_implemented.js
@@ -0,0 +1,23 @@
+var passing = function(done) {
+    done()
+}
+var failing = function(done) {
+    throw new Error("error");
+}
+module.exports.overlay = ['SI_8_1', 'SI_8_2', 'PM_6', 'SC_8_1', 'cr_213']
+
+org_provided_list_1 = {
+    'PM_6': passing
+}
+org_provided_list_2 = {
+    'SI_8_1': passing
+}
+
+module.exports.inherited_dict = {
+    'org1': org_provided_list_1,
+    'org2': org_provided_list_2
+}
+module.exports.implemented_dict = {
+    'PM_6': failing,
+    'SI_8_2': passing
+}
